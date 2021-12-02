bfb11c0259b4833b6c4736a58ed683b22a9b36a4
KINSHUK69 <71977053+KINSHUK69@users.noreply.github.com>
2021-12-02 23:18:05 +0530

    Update and rename QUICK SORT to QUICK_SORT.cpp

--- QUICK_SORT.cpp
+++ QUICK_SORT.cpp
@@ -1,24 +1,29 @@
 import<iostream>
 using namespace std;
-void swap(int *a,int *b){
+void swap(int *a,int *b)
+{
 int temp;
 temp=*a;
 *a=*b;
 *b=temp;
 }
-int random(int a[],int low ,int high){
+int random(int a[],int low ,int high)
+{
 int n,pvt,temp;
 n=rand();
 pvt=low+n%(high-low+1);
 swap(&a[high], &a[pvt]);
 return divide(a, low, high);
 }
-int divide(int a[], int low, int high){
+int divide(int a[], int low, int high)
+{
 int pivot, index, i;
 	index = low;
 	pivot = high;
-	for(i=low; i < high; i++)	{
-		if(a[i] < a[pivot]){
+	for(i=low; i < high; i++)	
+	{
+		if(a[i] < a[pivot])
+		{
 			swap(&a[i], &a[index]);
 			index++;
 		}
